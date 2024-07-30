fa34afab06594e1b9154da9eb6b76ebf70c1dfe0
aditya8850 <kalsotraaditya98@gmail.com>
2024-07-31 01:21:14 +0530

    components added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
-import { discount, robot } from '../assets'
-import { GetStarted } from '../components/index'
-import styles from '../style'
+import React from "react";
+import { discount, robot } from "../assets";
+import { GetStarted } from "../components/index";
+import styles from "../style";
 const Hero = () => (
-  <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY} md:py-5 width-{100vw} `}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center py-[6px] px-4  justify-center bg-discount-gradient rounded-[10px]   ">
-        <img src={discount} alt="discount" className="w-[43px] h-[43px] lg:w-[4rem] " />
-        <span className='text-white mr-2 font-semibold '>20% </span>
-        <span className='text-gray-400'> Discount For </span>
-        <span className='text-white ml-2 font-semibold'>1 Month </span>
+  <section
+    id="home"
+    className={`flex md:flex-row flex-col ${styles.paddingY} md:py-5 width-{100vw} `}
+  >
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className="flex flex-row items-center py-[6px] pr-4  justify-center bg-discount-gradient rounded-[10px]   ">
+        <img
+          src={discount}
+          alt="discount"
+          className="w-[43px] h-[43px] lg:w-[4rem] "
+        />
+        <span className="text-white mr-2 font-semibold ">20% </span>
+        <span className="text-gray-400"> Discount For </span>
+        <span className="text-white ml-2 font-semibold">1 Month </span>
       </div>
-      <div className='flex flex-row justify-between items-center w-full mt-0'>
-        <h1 className='font-poppins font-semibold text-white ss:text-[72px] text-[35px] ss:leading-[100px] leading-[3rem] md:text-left'>
-          <span className='block'>
-            The Next
-          </span>
-          <span className='block text-white text-gradient'>
-            Generation
-          </span>
+      <div className="flex flex-row justify-between items-center w-full mt-0">
+        <h1 className="font-poppins font-semibold text-white ss:text-[72px] text-[35px] ss:leading-[100px] leading-[3rem] md:text-left">
+          <span className="block">The Next</span>
+          <span className="block text-white text-gradient">Generation</span>
           {/* <span className='block text-gray-400'>
             Payment Method.
           </span> */}
         </h1>
-        <div className='flex lg:mr-60  '>
+        <div className="flex lg:mr-50  ">
           <GetStarted />
         </div>
       </div>
-      <h1 className='font-poppins font-semibold text-white ss:text-[72px] text-[52px] ss:leading-[100px] leading-[3rem] w-[100%] '>Payment Method.</h1>
-      <p className={`${styles.paragraph} max-w-[45rem] mt-5`}>Our Team of experts uses a methodology to idenitfy the credit cards most likely to fit your needs. We examine annual percentage rate,annual fees etc.</p>
+      <h1 className="font-poppins font-semibold text-white ss:text-[72px] text-[35px] ss:leading-[100px] leading-[3rem] bg-black-100 ">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[45rem] mt-5`}>
+        Our Team of experts uses a methodology to idenitfy the credit cards most
+        likely to fit your needs. We examine annual percentage rate,annual fees
+        etc.
+      </p>
     </div>
-    <div>Hello world</div>
+    <div className="relative ">
+      <img
+        src={robot}
+        alt="billing"
+        className="w-[100%] h-[100%] relative object-contain z-[5]
+        "
+      />
+      <div className=" pink__gradient  absolute  w-[17rem] h-[66%] top-[6rem] left-[7rem] z-[0]" />
+      <div className="white__gradient absolute bottom-40 w-[22rem] h-full z-[1]"/>
+      <div className="blue__gradient absolute  top-0 right-20 bottom-20 w-[10rem]  h-full z-[0]"/>
+    </div>
   </section>
-)
+);
 
-
-export default Hero
+export default Hero;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mt-7 mb-8 w-[100%]`}>
+    {stats.map((stat) => {
+      return (
+        <div key={stat.id} className={` w-[75%] flex md:flex-1 md:justify-evenly justify-center items-center font-poppins md:flex-row flex-column `}>
+          <h4 className="font-poppins font-semibold xs:text-[2.2rem] text-[2rem] xs:leading-[3.2rem] leading-[2.8rem] text-white  md:w-[50%] md:ml-[3.7rem]">{stat.value} </h4>
+          <p className="font-poppins  font-normal xs:text-[1.4rem] text-[1rem] xs:leading-[1.2rem] leading-[2.6rem] text-gradient  md:ml-[-2.4rem] md:w-[40%] ml-[0.7rem] w-full">{stat.title} </p>
+        </div>
+      );
+    })}
+  </section>
+);
 
-const Stats = () => {
-  return (
-    <div>Stats</div>
-  )
-}
-
-export default Stats
+export default Stats;
