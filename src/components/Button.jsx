95226462c7aa1c181f2adcdc993fb9f59f2a812a
aditya8850 <kalsotraaditya98@gmail.com>
2024-07-31 05:25:30 +0530

    scroll-trigger-bug-fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef ,useEffect} from "react";
 import styles from "../style";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const Button = ({ styles }) => {
   const buttonRef = useRef(null);
-  // Initialize GSAP animation and ScrollTrigger
-  const initializeGsap = () => {
+
+  useEffect(() => {
+    // Initialize GSAP animation and ScrollTrigger
     if (buttonRef.current) {
       gsap.fromTo(
         buttonRef.current,
@@ -19,17 +20,16 @@ const Button = ({ styles }) => {
           scrollTrigger: {
             trigger: buttonRef.current,
             start: "top 85%", // When the top of the trigger element reaches 85% of the viewport height
-            end: "top 95%", // When the top of the trigger element reaches 100% of the viewport height
+            end: "top 95%", // When the top of the trigger element reaches 95% of the viewport height
             scrub: 3, // Smoothly syncs the animation with the scroll position
           },
         }
       );
     }
-  };
+  }, []); // Empty dependency array ensures this runs once on mount
 
-  // Ensure GSAP initialization runs only once
 
-  initializeGsap();
+
 
   return (
     <button
